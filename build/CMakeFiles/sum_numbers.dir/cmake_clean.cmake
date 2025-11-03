5b764a044103645cdad82fe37f077f98a83d398a
polina <polytutynina@gmail.com>
2025-11-03 15:38:23 +0500

    Initial commit: ASM sum numbers program with working code

---
+++ build/CMakeFiles/sum_numbers.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sum_numbers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sum_numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
